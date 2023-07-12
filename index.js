d53cb5a80fcfe19332f6df91bbc81c60422bc8ee
Huzaifa Ghouri <120574411+HuzaifaGhouri08@users.noreply.github.com>
2023-07-12 08:42:38 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom"
+let root = document.getElementById('root');
+
+// ReactDOM.render( 
+//   <div>
+//   <h1>Sniper Eye FF</h1>
+//   <h3>Youtube Gaming Channel</h3>
+//   <p>Please Like And Share</p>
+//   </div>
+//   , root);
+
+// ReactDOM.render( 
+// <React.Fragment>
+// <h1>Sniper Eye FF</h1>
+// <h3>Youtube Gaming Channel</h3>
+// <p>Please Like And Share</p>
+// </React.Fragment>
+// , root);
+
+// ReactDOM.render( 
+//   [
+//   <h1>Sniper Eye FF</h1>,
+//   <h3>Youtube Gaming Channel</h3>,
+//   <p>Please Like And Share</p>
+//   ]
+//   , root);
+
+ReactDOM.render( 
+<>
+<h1>Sniper Eye FF</h1>
+<h3>Youtube Gaming Channel</h3>
+<p>Please Like And Share</p>
+</>
+, root);
